6b2ea755ad89362655b783a06aa7d78fc89cb97d
Eduardo Ruiz <eduardoruizm88@gmail.com>
2021-12-30 17:56:01 -0400

    change port

--- app/index.js
+++ app/index.js
@@ -57,7 +57,7 @@ fastify.post('/sendmail', (req, reply, next) => {
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen(3000, '0.0.0.0')
+    await fastify.listen(process.env.PORT || 3000, '0.0.0.0')
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
