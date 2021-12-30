7ec3ac4aceecff26de23d3bf02ab473226a9e85d
Eduardo Ruiz <eduardoruizm88@gmail.com>
2021-12-30 17:32:54 -0400

    change ip

--- app/index.js
+++ app/index.js
@@ -57,7 +57,7 @@ fastify.post('/sendmail', (req, reply, next) => {
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen(3000, '192.168.0.183')
+    await fastify.listen(3000, '0.0.0.0')
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
